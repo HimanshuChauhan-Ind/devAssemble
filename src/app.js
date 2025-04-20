7b3a6b5555aa450b7b52b334ea20c183f1662866
Himanshu Chauhan <him97cha@gmail.com>
2025-04-20 12:40:33 +0530

    Added the schema validations

--- src/app.js
+++ src/app.js
@@ -14,7 +14,61 @@ app.post("/signUp", async (req, res) => {
 
     res.send("User added successfully");
   } catch (err) {
-    console.log("Unable to add user", err);
+    console.log("Unable to add user: " + err);
+  }
+});
+
+app.get("/userbyemail", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    console.log("Unable to find user", err);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    console.log("Unable to find users", err);
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    console.log("Unable to update user", err);
+  }
+});
+
+app.delete("/removeUser", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User Delted Successfully");
+  } catch (err) {
+    console.log("Unable to delete the user", err);
+  }
+});
+
+app.use("/", (req, res, err) => {
+  if (err) {
+    res.send("Something went Wrong: " + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    about: {
+      type: String,
+      default: "I am a Developer",
+    },
+    gender: {
+      type: String,
+      validate(gender) {
+        if (!["male", "female", "others"].includes(gender)) {
+          throw new Error("Please select the correct gender");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
