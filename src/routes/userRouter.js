b3f6ae89a98bd7544f4cd76eee9072103ce6a3f1
Himanshu Chauhan <him97cha@gmail.com>
2025-04-22 16:48:38 +0530

    added the feed

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connection");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -42,4 +43,32 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     return res.status(400).json({ err });
   }
 });
+/*
+Feed api:
+show all the users of the app except the connection request that are in state: ignored, rejected
+*/
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequestSent = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { ttoUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsers = new Set();
+    connectionRequestSent.forEach((req) => {
+      hideUsers.add(req.fromUserId.toString());
+      hideUsers.add(req.toUserId.toString());
+    });
+
+    const feed = await User.find({
+      _id: { $nin: Array.from(hideUsers) },
+    }).select("firstName lastName");
+
+    res.json({ feed });
+  } catch (err) {
+    res.status(400).json({ ERROR: err });
+  }
+});
+
 module.exports = userRouter;
